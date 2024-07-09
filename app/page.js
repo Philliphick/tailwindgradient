b9b65160908df509f8ba66909e8784d1699ca703
Phillip H <phillip.hickinbotham@sunderlandsoftwarecity.com>
2024-07-09 16:04:57 +0100

    added copy to clipboard, and complimentary colour logic + UI

--- app/page.js
+++ app/page.js
@@ -2,10 +2,14 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { SketchPicker } from "react-color";
+import Alert from '@mui/material/Alert';
+
+
+
 
 export default function Home() {
 
-  const [colour, setColour] = useState("#816654");
+  const [colour, setColour] = useState("#546981");
   const [isGradient, setIsGradient] = useState(false);
   const [isDirection, setIsDirection] = useState(false);
   const [newInput, setNewInput] = useState("")
@@ -17,14 +21,17 @@ export default function Home() {
   const [showPicker1, setShowPicker1] = useState(false);
   const [showPicker2, setShowPicker2] = useState(false)
 
-  const [submit, setSubmit] = useState(false)
+  const [complimentary, setComplimentary] = useState("")
 
+  const [copyAlert, setCopyAlert] = useState(false)
   const [white, setWhite] = useState(false);
 
   const handleHide = (e) => {
     setHidden(!hidden)
     console.log(hidden)
   }
+
+  const cssCode = `linear-gradient(${direction}, ${colour}, ${colour2})`
 
   const handleColourChange = (e) => {
     setColour(e.hex);
@@ -60,24 +67,124 @@ export default function Home() {
     handleDirectionChange()
   }
 
-
-
-  // useEffect(() => {
-  //   setColour("#292D2E")
-  //   setColour2("#719599")
-  //   setDirection('50deg')
-  // }, [])
-
-  // useEffect(() => {
-  //   handleDirectionChange()
-  // }, [submit])
-
   const handleButton1 = (e) => {
     setShowPicker1(!showPicker1);
   }
 
   const handleButton2 = (e) => {
     setShowPicker2(!showPicker2);
+  }
+
+  const copyFunc = (e) => {
+    // const toCopy = "linear-gradient(${direction}, ${colour}, ${colour2})"
+    navigator.clipboard.writeText(cssCode)
+    console.log(cssCode)
+
+    setCopyAlert(true)
+  }
+
+  const handleComplimentary = (e) => {
+    setColour2(complimentary)
+  }
+
+  // finding the complimentary colour vv
+
+
+  useEffect(() => {
+    hexToHSL(colour)
+    console.log(complimentary, "this is complimentary")
+  }, [colour])
+  // call vv in a useEffect? 
+
+  function hexToHSL(H) {
+    // Convert hex to RGB first
+    let r = 0, g = 0, b = 0;
+    if (H.length == 4) {
+      r = "0x" + H[1] + H[1];
+      g = "0x" + H[2] + H[2];
+      b = "0x" + H[3] + H[3];
+    } else if (H.length == 7) {
+      r = "0x" + H[1] + H[2];
+      g = "0x" + H[3] + H[4];
+      b = "0x" + H[5] + H[6];
+    }
+    // Then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r,g,b),
+        cmax = Math.max(r,g,b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+  
+    if (delta == 0)
+      h = 0;
+    else if (cmax == r)
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      h = (b - r) / delta + 2;
+    else
+      h = (r - g) / delta + 4;
+  
+    h = Math.round(h * 60);
+  
+    if (h < 0)
+      h += 360;
+  
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    // added 180 to find complimentary 
+    h += 180;
+
+    HSLToHex(h,s,l)
+
+    // return "hsl(" + h + "," + s + "%," + l + "%)";
+  }
+
+  function HSLToHex(h,s,l) {
+    s /= 100;
+    l /= 100;
+  
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c/2,
+        r = 0,
+        g = 0, 
+        b = 0; 
+  
+    if (0 <= h && h < 60) {
+      r = c; g = x; b = 0;
+    } else if (60 <= h && h < 120) {
+      r = x; g = c; b = 0;
+    } else if (120 <= h && h < 180) {
+      r = 0; g = c; b = x;
+    } else if (180 <= h && h < 240) {
+      r = 0; g = x; b = c;
+    } else if (240 <= h && h < 300) {
+      r = x; g = 0; b = c;
+    } else if (300 <= h && h < 360) {
+      r = c; g = 0; b = x;
+    }
+    // Having obtained RGB, convert channels to hex
+    r = Math.round((r + m) * 255).toString(16);
+    g = Math.round((g + m) * 255).toString(16);
+    b = Math.round((b + m) * 255).toString(16);
+  
+    // Prepend 0s, if necessary
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+    setComplimentary("#" + r + g + b)
+    console.log(complimentary)
+    return "#" + r + g + b;
   }
 
   return (
@@ -103,7 +210,7 @@ export default function Home() {
 
         <div className="flex flex-row gap-10">
           <div className="flex flex-col gap-4">
-            <button className="border-2 p-4 rounded-xl shadow-2xl dark:text-white hover:scale-110" style={{ background: colour }} onClick={handleButton1}>Colour One</button>
+            <button className="border-2 p-4 rounded-xl shadow-2xl dark:text-white hover:scale-110 text-2xl" style={{ background: colour }} onClick={handleButton1}>Colour One</button>
 
             {showPicker1 ? (<SketchPicker
               color={colour}
@@ -113,11 +220,13 @@ export default function Home() {
 
           {isGradient ? (
             <div className="flex flex-col gap-4">
-              <button className="border-2 p-4 rounded-xl shadow-2xl dark:text-white hover:scale-110" style={{ background: colour2 }} onClick={handleButton2}>Colour Two</button>
+              <button className="border-2 p-4 rounded-xl shadow-2xl dark:text-white hover:scale-110 text-2xl" style={{ background: colour2 }} onClick={handleButton2}>Colour Two</button>
               {showPicker2 ? (<SketchPicker
                 color={colour2}
                 onChangeComplete={handleColour2Change}
-              />) : (null)}
+              />) : (<div>
+                <button style={{background: complimentary}} className="p-2 rounded-lg"  onClick={handleComplimentary}>Complimentary Colour</button>
+              </div>)}
 
 
             </div>) : (null)}
@@ -131,14 +240,34 @@ export default function Home() {
           <form action={handleSubmit}>
             <label for="direction" className="dark:text-white">Gradient Direction 1 - 360</label>
             <input type="text" id="direction" placeholder="(1 - 360)" onChange={handleInputChange} className="mx-4 text-center w-1/3"></input>
-            <input type="submit" className="border-2 border-solid border-black p-2 rounded-lg dark:text-white hover:scale-110"></input>
+            <input type="submit" className="border-2 border-solid border-gray-800 bg-gray-400 p-2 rounded-lg dark:text-white hover:scale-110 active:bg-gray-500"></input>
           </form>
-
         </div>
+
+        <section className="w-fit p-2 flex flex-col gap-2">
+          {/* add func to copy tailwind code for this button */}
+          {/* <div className="">
+            <button className="bg-gray-300 hover:bg-gray-400 p-2 rounded-lg">
+              <div className="flex flex-row gap-4"><h1>Tailwind Code</h1>
+            <svg className="size-6" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m6 18h-3c-.48 0-1-.379-1-1v-14c0-.481.38-1 1-1h14c.621 0 1 .522 1 1v3h3c.621 0 1 .522 1 1v14c0 .621-.522 1-1 1h-14c-.48 0-1-.379-1-1zm7.25-4.75h-2.5c-.414 0-.75.336-.75.75s.336.75.75.75h2.5v2.5c0 .414.336.75.75.75s.75-.336.75-.75v-2.5h2.5c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-2.5v-2.5c0-.414-.336-.75-.75-.75s-.75.336-.75.75zm3.25-7.25v-2.5h-13v13h2.5v-9.5c0-.481.38-1 1-1z" fill-rule="nonzero"/></svg>
+              </div>
+            </button>
+          </div> */}
+          <div className="">
+            <button onClick={copyFunc} className="bg-gray-300 hover:bg-gray-400 p-2 rounded-lg active:bg-green-300 visited:bg-green-400">
+              <div className="flex flex-row gap-4"><h1>CSS Code</h1>
+            <svg className="size-6" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m6 18h-3c-.48 0-1-.379-1-1v-14c0-.481.38-1 1-1h14c.621 0 1 .522 1 1v3h3c.621 0 1 .522 1 1v14c0 .621-.522 1-1 1h-14c-.48 0-1-.379-1-1zm7.25-4.75h-2.5c-.414 0-.75.336-.75.75s.336.75.75.75h2.5v2.5c0 .414.336.75.75.75s.75-.336.75-.75v-2.5h2.5c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-2.5v-2.5c0-.414-.336-.75-.75-.75s-.75.336-.75.75zm3.25-7.25v-2.5h-13v13h2.5v-9.5c0-.481.38-1 1-1z" fill-rule="nonzero"/></svg>
+              <p className="">linear-gradient ({direction}, {colour}, {colour2})
+              </p>
+              </div>
+            </button>
+          </div>
+
+        </section>
 
       </>) : (null)} 
       {/*  */}
-      <div className="flex flex-row gap-12 fixed top-10 left-12">
+      <div className="flex flex-row gap-12 fixed top-6 left-12">
         <div className="flex flex-col text-center">
           <h3 className="text-lg pb-2 dark:text-white">Colour One</h3>
           <h2 className="ring-4 p-4 dark:text-white text-xl">{colour}</h2>
@@ -151,6 +280,10 @@ export default function Home() {
           : null}
       </div>
 
+      {/* {copyAlert ? setTimeout(() => {
+  console.log("Delayed for 1 second.");
+}, "1000")
+     : (null)} */}
 
     </main>
   );
